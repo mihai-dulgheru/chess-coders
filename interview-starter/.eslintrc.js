38c147528312fc5d947c50e90f3dd4a9016d5a84
mihai-dulgheru <63505058+mihai-dulgheru@users.noreply.github.com>
2022-07-28 16:36:27 +0300

    Formatted the code with Prettier

--- interview-starter/.eslintrc.js
+++ interview-starter/.eslintrc.js
@@ -2,21 +2,21 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-    es2020: true
+    es2020: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react'],
   rules: {
     'no-warning-comments': 'warn',
     'react/no-unescaped-entities': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
-  }
-}
+    'react/prop-types': 'off',
+  },
+};
